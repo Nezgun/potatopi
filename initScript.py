ac930650dedea104432b831e83bfa96cea02bf47
nezgun <mhoffman.cheb@gmail.com>
2019-12-05 14:08:14 -0500

    added more comments to pyscript

--- initScript.py
+++ initScript.py
@@ -1,14 +1,20 @@
+
+#function to find the inet address from an existing inet.txt file
+#
+
 def ip_search():
-    f = open("inet.txt", "r")
-    skip = 0
+    f = open("inet.txt", "r") #open inet.txt in reading mode
+    skip = 0 #way of skipping over undesired inets
     while(1):
         line = f.readline()
-        if("inet" in line and skip == 2):
+        if("inet" in line and skip == 2): #skipping undesired ones, and then will take selected.
             target = line.split()
             f.close()
             return target[1]
         elif("inet" in line):    
             skip += 1
+
+#creates or updates file "inet_address.txt" and writes the inet to it.
 
 f = open("inet_address.txt", "w+")
 f.write(ip_search)
