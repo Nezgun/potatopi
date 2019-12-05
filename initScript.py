c11a2e11ccb0eb6b151f16d01954a426dbf77ed5
nezgun <mhoffman.cheb@gmail.com>
2019-12-05 13:41:13 -0500

    updated py script

--- initScript.py
+++ initScript.py
@@ -1,12 +1,15 @@
 def ip_search():
-    f = open("inetinfo.txt", "r")
+    f = open("inet.txt", "r")
     skip = 0
     while(1):
         line = f.readline()
         if("inet" in line and skip == 2):
             target = line.split()
+            f.close()
             return target[1]
         elif("inet" in line):    
             skip += 1
 
-print(ip_search())
+f = open("inet_address.txt", "w+")
+f.write(ip_search)
+f.close()
