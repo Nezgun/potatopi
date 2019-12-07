1af0919ecd3551fe9fabfc6a0621481aecd3c441
nezgun <mhoffman.cheb@gmail.com>
2019-12-07 14:05:38 -0500

    updated to send to multiple people

--- initScript.py
+++ initScript.py
@@ -6,12 +6,15 @@ from email.mime.text import MIMEText
 from email.utils import formatdate
 from email import encoders
 
+recipients = ['mhoffman.cheb@gmail.com', 'jhengenius@student.monroecc.edu', 'mhoffman.cheb@mail.ru'] #recipient list
+
 ### Function to send the email ###
-##AUTHOR: Dan Aldred | Github: TeCoEd | Link https://github.com/TeCoEd | Additional comments by Michael Hoffman
-def send_an_email():
-    toaddr = 'recipient'    #Who email is sent to  
-    me = 'sender' #Who email is sent from 
-    subject = "subject" #Subject
+##AUTHOR: Dan Aldred | Github: TeCoEd | Link https://github.com/TeCoEd | 
+## Additional comments and modifications by Michael Hoffman
+def send_an_email(recipient):
+    toaddr = recipient    #recipient
+    me = 'mhoffman.cheb@gmail.com' #sender
+    subject = "inet address" #subject
 
     msg = MIMEMultipart()
     msg['Subject'] = subject
@@ -21,9 +24,9 @@ def send_an_email():
     #msg.attach(MIMEText(text))
 
     part = MIMEBase('application', "octet-stream")
-    part.set_payload(open("inet_address.txt", "rb").read()) #attachment file
+    part.set_payload(open("inet_address.txt", "rb").read()) #file to be sent
     encoders.encode_base64(part)
-    part.add_header('Content-Disposition', 'attachment; filename="inet_address.txt"') #attachment file
+    part.add_header('Content-Disposition', 'attachment; filename="inet_address.txt"') #file to be sent
     msg.attach(part)
 
     try:
@@ -31,13 +34,14 @@ def send_an_email():
        s.ehlo()
        s.starttls()
        s.ehlo()
-       s.login(user = 'username', password = 'password') #sender + credentials (use app password for gmail)
+       s.login(user = 'mhoffman.cheb@gmail.com', password = '') #includes sender email and password
        #s.send_message(msg)
-       s.sendmail(me, toaddr, msg.ashqibtjgvktfckbxj_string())
+       s.sendmail(me, toaddr, msg.as_string())
        s.quit()
+    #except:
+    #   print ("Error: unable to send email")
     except:
-       print ("Error: unable to send email")
-    return
+          print ("Error")
     
     #except SMTPException as error:
 #     print ("Error")
@@ -60,5 +64,6 @@ def ip_search():
 f = open("inet_address.txt", "w+")
 f.write(ip_search())
 f.close()
-send_an_email()
+for person in recipients:
+    send_an_email(person)
 
